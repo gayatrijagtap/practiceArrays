aae8d7bf32f15306d9a0347431c2f68eb74db109
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 14:20:50 +0530

    code refactored.
    library file path changed.

--- testing/testCountingEvenNumbers.js
+++ testing/testCountingEvenNumbers.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const countEvens = require('./countingEvenNumbers.js').countEvens;
+const countEvens = require('../arrayLibrary.js').countEvens;
 assert.equal(countEvens([1,2,3,4,5,6,7,8,9]),4);
 assert.equal(countEvens([1,2,3]),1);
 assert.equal(countEvens([1]),0);
