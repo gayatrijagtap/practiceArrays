a83a4feb1495fea5f7fdaae22b690f5360bed77d
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 14:17:39 +0530

    code modified.
    library file path changed.

--- testing/testAscendingOrder.js
+++ testing/testAscendingOrder.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const checkAscendingOrder=require('./ascendingOrder.js').checkAscendingOrder;
+const checkAscendingOrder=require('../arrayLibrary.js').checkAscendingOrder;
 assert.equal(checkAscendingOrder([1,2,3,4,5]),true);
 assert.equal(checkAscendingOrder([1,2,4,3,8]),false);
 assert.equal(checkAscendingOrder([0,3,6,8,9]),true);
