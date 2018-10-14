8d020b796154a5d11b6630498180b9b2f8df0729
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 16:02:44 +0530

    code refactored.
    library file path changed.

--- testing/testReverseFibonnaci.js
+++ testing/testReverseFibonnaci.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const reverseFibonnaci = require('./reverseFibonnaci.js').main;
+const reverseFibonnaci = require('../arrayLibrary.js').createReverseFibo;
 assert.deepEqual(reverseFibonnaci(8),[13, 8, 5, 3, 2, 1, 1, 0]);
 assert.deepEqual(reverseFibonnaci(5),[3, 2, 1, 1, 0]);
 assert.deepEqual(reverseFibonnaci(8),[13, 8, 5, 3, 2, 1, 1, 0]);
