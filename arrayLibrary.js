bce8c95387ed560fbe05059c95f4d878e31681bd
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 16:07:45 +0530

    new function added.
    function for extracting the even numbers from an array is added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -204,6 +204,19 @@ const createReverseFibo = function(limit) {
   return reverse(series);
 }
 
+const printEvenSeries = function(numbers) {
+  let series = [];
+  let seriesIndex = 0;
+  for(let i=0;i<numbers.length; i++) {
+    if(numbers[i]%2 == 0) {
+      series[seriesIndex]=numbers[i];
+      seriesIndex++;
+    }
+  }
+  return series;
+}
+exports.evenSeries=printEvenSeries;
+
 exports.createReverseFibo = createReverseFibo;
 
 exports.reverse = reverse;
