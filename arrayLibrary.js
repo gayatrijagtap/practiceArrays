e36b84e5eab158f91fddc9ca998cdc9ab5357bf4
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 16:20:21 +0530

    new function added.
    function for performing the sum of all the elements in an array is
    added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -248,6 +248,16 @@ const smallestNumber = function(numbers) {
   return smallestNum;
 }
 
+const sumOfArrayElement = function(numbers) {
+  let sum=0;
+  for(let i=0; i<numbers.length; i++) {
+    sum = sum + numbers[i];
+  }
+  return sum;
+}
+
+exports.sum=sumOfArrayElement;
+
 exports.smallestNum=smallestNumber;
 
 exports.oddSeries=printOddSeries
