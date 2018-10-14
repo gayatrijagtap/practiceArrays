6a701b3d76a457e1a921dbd33e2fd9664b0ce2d4
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 14:51:12 +0530

    new function added.
    function for finding greatest number amongst the array elements is
    added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -127,6 +127,17 @@ const genAverage = function(numbers) {
   return sum/numbers.length;
 }
 
+const greatestNumber = function(numbers) {
+  let greatestNum = numbers[0];
+  for(let i=0; i<numbers.length; i++) {
+    if(numbers[i]>greatestNum) {
+      greatestNum = numbers[i];
+    }
+  }
+  return greatestNum;
+}
+exports.greatestNum=greatestNumber;
+
 exports.genAverage=genAverage;
 
 exports.extractDigits = extractDigits;
