a7109412be02a900dd1c9eb4f77f7b0e9cc9ece0
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 16:06:52 +0530

    code refactored.
    library file path changed.

--- testing/testEvenSeries.js
+++ testing/testEvenSeries.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
-let evenSeries = require('./selectingEvenNumbers.js').evenSeries;
+let evenSeries = require('../arrayLibrary.js').evenSeries;
 
 let numbers = [1,2,3,4,5];
-console.log(assert.deepEqual(evenSeries(numbers),[2,4]));
-console.log(assert.deepEqual(evenSeries([3,2]),[2]));
-console.log(assert.deepEqual(evenSeries([6,4,2]),[6,4,2]));
-console.log(assert.deepEqual(evenSeries([0,1,2,3,4]),[0,2,4]));
+assert.deepEqual(evenSeries(numbers),[2,4]);
+assert.deepEqual(evenSeries([3,2]),[2]);
+assert.deepEqual(evenSeries([6,4,2]),[6,4,2]);
+assert.deepEqual(evenSeries([0,1,2,3,4]),[0,2,4]);
