5d89b3f37518d9e02d4dfbc81addc58b616a4132
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 13:24:58 +0530

    code modified.
    function to find number below certain threshold in an array is added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -8,4 +8,16 @@ const numbersAboveThreshold = function(numbers,number) {
   return count;
 }
 
+const numbersBelowThreshold = function(numbers,number) {
+  let count = 0;
+  for(let i=0; i<numbers.length; i++) {
+    if(numbers[i]<number) {
+      count++;
+    }
+  }
+  return count;
+}
+
+exports.numbersBelowThreshold = numbersBelowThreshold;
+
 exports.numbersAboveThreshold = numbersAboveThreshold;
