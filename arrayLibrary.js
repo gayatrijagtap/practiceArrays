0f7461a4505e86610b7eb7264ea63fbd2f980323
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 15:10:39 +0530

    new function added.
    function for reversing the array elements is added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -180,6 +180,18 @@ const partitionArray = function(numbers,number) {
   return partition;
 }
 
+const reverse = function(numbers) {
+  let index=0;
+  let reversedArray = [];
+  for(let i=numbers.length-1; i>=0; i--) {
+    reversedArray[index]=numbers[i];
+    index++;
+  }
+  return(reversedArray);
+}
+
+exports.reverse = reverse;
+
 exports.partitionArray = partitionArray;
 
 exports.mapLengths = mapLengths;
