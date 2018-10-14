85a19a508f38f4fc95a7a684777c616c8917472d
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 14:47:03 +0530

    new function added.
    function for finding an average of the numbers in an array is added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -119,6 +119,16 @@ const extractDigits = function(number) {
   return numbers;
 }
 
+const genAverage = function(numbers) {
+  let sum = 0;
+  for(let i=0; i<numbers.length; i++) {
+    sum = sum+numbers[i];
+  }
+  return sum/numbers.length;
+}
+
+exports.genAverage=genAverage;
+
 exports.extractDigits = extractDigits;
 
 exports.checkDescendingOrder=checkDescendingOrder;
