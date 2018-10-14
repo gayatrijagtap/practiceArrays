9a3e3ed44f19b764eebaa99ef63551eca533735f
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 14:54:14 +0530

    new function added.
    function for finding the position of an element in an array is added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -136,6 +136,17 @@ const greatestNumber = function(numbers) {
   }
   return greatestNum;
 }
+
+const indexOfNumber = function(numbers,number) {
+  for(let i=0; i<numbers.length; i++) {
+    if(numbers[i] == number) {
+      return i;
+    }
+  }
+  return "unmatched";
+}
+
+exports.indexOfNumber = indexOfNumber;
 exports.greatestNum=greatestNumber;
 
 exports.genAverage=genAverage;
