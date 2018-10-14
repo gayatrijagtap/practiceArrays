655f286fcc5fe98c473436464b03933732193f70
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-14 16:11:21 +0530

    new function added.
    function for selecting every second element in an array is added.

--- arrayLibrary.js
+++ arrayLibrary.js
@@ -215,6 +215,19 @@ const printEvenSeries = function(numbers) {
   }
   return series;
 }
+
+const selectSecondElement = function(numbers) {
+  let selectedElements = [];
+  let index = 0;
+  for(let i=0; i<numbers.length; i= i+2) {
+    selectedElements[index]=numbers[i];
+    index++;
+  }
+  return selectedElements;
+}
+
+exports.selectElements=selectSecondElement;
+
 exports.evenSeries=printEvenSeries;
 
 exports.createReverseFibo = createReverseFibo;
