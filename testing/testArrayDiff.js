e290a60dc71fe19e49aaef772b0f6abe85819b22
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-18 15:52:57 +0530

    folder modified.

--- testing/testArrayDiff.js
+++ testing/testArrayDiff.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
-const genDifference = require('../arrayLibrary.js').genDifference;
-assert.deepEqual(genDifference([1,2,3,4],[3,4,5,6]),[1,2]);
-assert.deepEqual(genDifference([1,2],[1,2,5,6]),[]);
-assert.deepEqual(genDifference([1,2,3,4],[1,2,3,4]),[]);
-assert.deepEqual(genDifference([3,4,5,6],[5,6]),[3,4]);
-assert.deepEqual(genDifference([4,5,6,7],[3,4,5,6]),[7]);
+const generateDifference = require('../arrayLibrary.js').generateDifference;
+assert.deepEqual(generateDifference([1,2,3,4],[3,4,5,6]),[1,2]);
+assert.deepEqual(generateDifference([1,2],[1,2,5,6]),[]);
+assert.deepEqual(generateDifference([1,2,3,4],[1,2,3,4]),[]);
+assert.deepEqual(generateDifference([3,4,5,6],[5,6]),[3,4]);
+assert.deepEqual(generateDifference([4,5,6,7],[3,4,5,6]),[7]);
