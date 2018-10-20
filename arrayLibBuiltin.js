526e24085ed64eed88f8643073868e089a3d6bd2
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-20 10:14:36 +0530

    function for extracting odd elements in an array using filter.

--- arrayLibBuiltin.js
+++ arrayLibBuiltin.js
@@ -1,6 +1,17 @@
+//-----------------extracting even numbers in an array-------------------------
 const selectEvens = function(numbers) {
  let evenElements = numbers.filter( function(element) { return element%2 == 0 } )
   return evenElements;
 }
 
 exports.selectEvens = selectEvens;
+
+//------------------extracting odd numbers in an array--------------------------
+
+const selectOdds = function(numbers) {
+ let oddElements = numbers.filter( function(element) { return element%2 != 0 } )
+  return oddElements;
+}
+
+exports.selectOdds = selectOdds;
+
